226cefb96141fe5895096bb8646dc03de1a1d809
Philip Kuryloski <pjk25@cornell.edu>
2011-03-22 15:14:34 -0700

    Initial commit of generated project

---
+++ src/edu/berkeley/waveclientsample/WaveClientSample.java
@@ -0,0 +1,15 @@
+package edu.berkeley.waveclientsample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WaveClientSample extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
