895e1f0f79591138713847a632a60c446769e92e
Philip Kuryloski <pjk25@cornell.edu>
2011-04-03 02:27:46 +0200

    Added AllTests.java
    For running all tests

---
+++ tests/src/edu/berkeley/waveclientsample/AllTests.java
@@ -0,0 +1,17 @@
+package edu.berkeley.androidwave;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import android.test.suitebuilder.TestSuiteBuilder;
+
+/**
+ * A test suite containing all tests for my application.
+ * 
+ * adb shell am instrument -w edu.berkeley.waveclientsample.tests/android.test.InstrumentationTestRunner
+ */
+public class AllTests extends TestSuite {
+    public static Test suite() {
+        return new TestSuiteBuilder(AllTests.class).includeAllPackagesUnderHere().build();
+    }
+}
