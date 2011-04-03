8f02aecd6a7c076bb2202d764c7832dd1383d2a8
Philip Kuryloski <pjk25@cornell.edu>
2011-04-03 02:22:42 +0200

    Added test project

---
+++ tests/src/edu/berkeley/waveclientsample/WaveClientSampleTest.java
@@ -0,0 +1,21 @@
+package edu.berkeley.waveclientsample;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * This is a simple framework for a test of an Application.  See
+ * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
+ * how to write and extend Application tests.
+ * <p/>
+ * To run this test, you can type:
+ * adb shell am instrument -w \
+ * -e class edu.berkeley.waveclientsample.WaveClientSampleTest \
+ * edu.berkeley.waveclientsample.tests/android.test.InstrumentationTestRunner
+ */
+public class WaveClientSampleTest extends ActivityInstrumentationTestCase2<WaveClientSample> {
+
+    public WaveClientSampleTest() {
+        super("edu.berkeley.waveclientsample", WaveClientSample.class);
+    }
+
+}
