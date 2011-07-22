f3b4348c7818e6c00aa19b0962809520c5ba86dd
Philip Kuryloski <pjk25@cornell.edu>
2011-07-22 11:41:54 -0700

    Updated WaveClientSample to support multiple recipes
    Recipe list is compiled in, but can be replaced
    with storage in preferences later.  Log text now
    shows 10 most recent pieces of received output
    data.
    
    Note: High rate data still fails to update quickly
    on screen.

--- src/edu/berkeley/waveclientsample/WaveClientSample.java
+++ src/edu/berkeley/waveclientsample/WaveClientSample.java
@@ -13,20 +13,25 @@ import edu.berkeley.androidwave.waveclient.IWaveRecipeOutputDataListener;
 import edu.berkeley.androidwave.waveclient.ParcelableWaveRecipeOutputData;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WaveClientSample extends Activity {
     
@@ -36,13 +41,21 @@ public class WaveClientSample extends Activity {
     private static final String ACTION_DID_AUTHORIZE = "edu.berkeley.androidwave.intent.action.DID_AUTHORIZE";
     private static final String ACTION_DID_DENY = "edu.berkeley.androidwave.intent.action.DID_DENY";
     private static final int REQUEST_CODE_AUTH = 1;
-    private final String RECIPE_ID = "edu.berkeley.waverecipe.AccelerometerMagnitude";
     private final String API_KEY = "snathtosoeaseseoadrc,h.bmte";
+    protected final int VISIBLE_LOG_LINES = 8;
     
     private IWaveServicePublic mWaveService;
     private boolean mBound;
     
-    Button authRequestButton;
+    // NOTE: ideally, recipeIds would be stored in preferences, and editable by the user
+    protected String[] recipeIds = {"edu.berkeley.waverecipe.AccelerometerMagnitude",
+                                    "edu.berkeley.waverecipe.Kcal"};
+    protected String chosenRecipeId;
+    protected List<String> logLines;
+    
+    protected boolean logging;
+    
+    Button startButton;
     TextView messageTextView;
     
     /** Called when the activity is first created. */
@@ -53,23 +66,40 @@ public class WaveClientSample extends Activity {
         setTitle(R.string.main_activity_name);
         setContentView(R.layout.main);
         
-        authRequestButton = (Button) findViewById(R.id.auth_request_button);
+        startButton = (Button) findViewById(R.id.start_button);
         messageTextView = (TextView) findViewById(R.id.message_textview);
         
+        logging = false;
+        
+        logLines = new ArrayList<String>(VISIBLE_LOG_LINES);
+        
         // disable the button until we have connected to the service
-        authRequestButton.setEnabled(false);
-        authRequestButton.setOnClickListener(authRequestListener);
+        startButton.setEnabled(false);
+        startButton.setOnClickListener(startButtonListener);
 
         // connect to the service
         Intent i = new Intent(ACTION_WAVE_SERVICE);
-        if (bindService(i, mConnection, Context.BIND_AUTO_CREATE)) {
-            mBound = true;
-            Toast.makeText(WaveClientSample.this, "Connected to WaveService", Toast.LENGTH_SHORT).show();
-        } else {
-            Log.d(getClass().getSimpleName(), "Could not bind with "+i);
-            // TODO: replace this Toast with a dialog that allows quitting
-            Toast.makeText(WaveClientSample.this, "Could not connect to the WaveService!", Toast.LENGTH_SHORT).show();
-            messageTextView.setText("ERROR:\n\nFailed to bind to the WaveService.\n\nIs AndroidWave installed on this device?\n\nPlease address this issue and restart this Application.");
+        try {
+            if (bindService(i, mConnection, Context.BIND_AUTO_CREATE)) {
+                mBound = true;
+                Toast.makeText(WaveClientSample.this, "Connected to WaveService", Toast.LENGTH_SHORT).show();
+            } else {
+                Log.d(getClass().getSimpleName(), "Could not bind with "+i);
+                // TODO: replace this Toast with a dialog that allows quitting
+                Toast.makeText(WaveClientSample.this, "Could not connect to the WaveService!", Toast.LENGTH_SHORT).show();
+                messageTextView.setText("ERROR:\n\nFailed to bind to the WaveService.\n\nIs AndroidWave installed on this device?\n\nPlease address this issue and restart this Application.");
+            }
+        } catch (SecurityException se) {
+            Log.d(TAG, "SecurityException on bind", se);
+            AlertDialog.Builder builder = new AlertDialog.Builder(WaveClientSample.this);
+            builder.setMessage("Security Exception: "+se)
+                   .setCancelable(false)
+                   .setPositiveButton("Quit", new DialogInterface.OnClickListener() {
+                       public void onClick(DialogInterface dialog, int id) {
+                           WaveClientSample.this.finish();
+                       }
+                   });
+            AlertDialog alert = builder.show();
         }
     }
     
@@ -77,10 +107,12 @@ public class WaveClientSample extends Activity {
     protected void onStop() {
         super.onStop();
         
-        try {
-            mWaveService.unregisterRecipeOutputListener(API_KEY, RECIPE_ID);
-        } catch (RemoteException e) {
-            Log.d("WaveClientSample", "lost connection to the service");
+        if (logging) {
+            try {
+                mWaveService.unregisterRecipeOutputListener(API_KEY, chosenRecipeId);
+            } catch (RemoteException e) {
+                Log.d("WaveClientSample", "lost connection to the service");
+            }
         }
 
         if (mBound) {
@@ -89,21 +121,25 @@ public class WaveClientSample extends Activity {
         }
     }
     
-    private void setButtonForWaveUi() {
-        authRequestButton.setText("Deauthorize in Wave UI");
-        authRequestButton.setOnClickListener(waveUiRequestListener);
-        authRequestButton.setEnabled(true);
-    }
-    
-    private void beginStreamingRecipeData() {
+    protected void startLogging() {
+        logging = true;
+        
+        messageTextView.setText("Logging started...");
+        
         try {
-            boolean didRegister = mWaveService.registerRecipeOutputListener(API_KEY, RECIPE_ID, outputListener);
+            boolean didRegister = mWaveService.registerRecipeOutputListener(API_KEY, chosenRecipeId, outputListener);
             if (!didRegister) {
                 Toast.makeText(WaveClientSample.this, "Error requesting recipe data stream.", Toast.LENGTH_SHORT).show();
             }
         } catch (RemoteException e) {
             Log.d("WaveClientSample", "lost connection to the service");
         }
+    }
+    
+    private void setButtonForWaveUi() {
+        startButton.setText("Deauthorize in Wave UI");
+        startButton.setOnClickListener(waveUiRequestListener);
+        startButton.setEnabled(true);
     }
     
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -113,7 +149,7 @@ public class WaveClientSample extends Activity {
                     Toast.makeText(WaveClientSample.this, "Authorization Successful!", Toast.LENGTH_SHORT).show();
                     
                     setButtonForWaveUi();
-                    beginStreamingRecipeData();
+                    startLogging();
                 } else {
                     Toast.makeText(WaveClientSample.this, "Authorization Denied!", Toast.LENGTH_SHORT).show();
                 }
@@ -123,22 +159,40 @@ public class WaveClientSample extends Activity {
         }
     }
     
-    private OnClickListener authRequestListener = new OnClickListener() {
+    private OnClickListener startButtonListener = new OnClickListener() {
         public void onClick(View v) {
-            try {
-                // get an auth intent from the service
-                Intent i = mWaveService.getAuthorizationIntent(RECIPE_ID, API_KEY);
+            startButton.setEnabled(false);
             
-                // then run it looking for a result
-                try {
-                    startActivityForResult(i, REQUEST_CODE_AUTH);
-                } catch (ActivityNotFoundException anfe) {
-                    anfe.printStackTrace();
-                    Toast.makeText(WaveClientSample.this, "Error launching authorization UI", Toast.LENGTH_SHORT).show();
+            // let the user choose a recipe
+            AlertDialog.Builder builder = new AlertDialog.Builder(WaveClientSample.this);
+            builder.setTitle("Select Recipe");
+            builder.setItems(recipeIds, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int item) {
+                    chosenRecipeId = recipeIds[item];
+                    
+                    try {
+                        if (mWaveService.isAuthorized(API_KEY, chosenRecipeId)) {
+                            startLogging();
+                            setButtonForWaveUi();
+                        } else {
+                            // get an auth intent from the service
+                            Intent i = mWaveService.getAuthorizationIntent(chosenRecipeId, API_KEY);
+            
+                            // then run it looking for a result
+                            try {
+                                startActivityForResult(i, REQUEST_CODE_AUTH);
+                            } catch (ActivityNotFoundException anfe) {
+                                anfe.printStackTrace();
+                                Toast.makeText(WaveClientSample.this, "Error launching authorization UI", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    } catch (RemoteException re) {
+                        Log.d(TAG, "lost connection to the service", re);
+                        Toast.makeText(WaveClientSample.this, "Lost connection to WaveService", Toast.LENGTH_SHORT).show();
+                    }
                 }
-            } catch (RemoteException e) {
-                Log.d("WaveClientSample", "lost connection to the service");
-            }
+            });
+            AlertDialog alert = builder.show();
         }
     };
     
@@ -161,25 +215,7 @@ public class WaveClientSample extends Activity {
         public void onServiceConnected(ComponentName className, IBinder service) {
             mWaveService = IWaveServicePublic.Stub.asInterface(service);
             
-            // check if we are authorized for the recipe and update the UI
-            //  - if we are already authorized, let the user switch to the WaveUI
-            //    to deauthorize
-            //  - if we are not authorized, let the user request it
-            try {
-                // enable the button now that the service is connected
-                authRequestButton.setEnabled(true);
-                
-                if (mWaveService.isAuthorized(API_KEY, RECIPE_ID)) {
-                    Toast.makeText(WaveClientSample.this, "Already authorized for Recipe\n"+RECIPE_ID, Toast.LENGTH_SHORT).show();
-                    
-                    // reconfigure the UI after auth
-                    setButtonForWaveUi();
-                    // we should request that data be streamed and start displaying it in the log
-                    beginStreamingRecipeData();
-                }
-            } catch (RemoteException re) {
-                Log.d(TAG, "lost connection to the service", re);
-            }
+            startButton.setEnabled(true);
         }
         
         public void onServiceDisconnected(ComponentName className) {
@@ -191,11 +227,16 @@ public class WaveClientSample extends Activity {
         public void receiveWaveRecipeOutputData(ParcelableWaveRecipeOutputData wrOutput) {
             // Log.v(TAG, "IWaveRecipeOutputDataListener.Stub got "+wrOutput);
             // update the log text
-            final ParcelableWaveRecipeOutputData o = wrOutput;
-            runOnUiThread(new Runnable() {
+            // final ParcelableWaveRecipeOutputData o = wrOutput;
+            synchronized(this) {
+                if (logLines.size() > VISIBLE_LOG_LINES - 1) {
+                    logLines.remove(0);
+                }
+                logLines.add(String.format("time: %d, values: %s", wrOutput.getTime(), wrOutput.valuesAsMap()));
+            }
+            messageTextView.post(new Runnable() {
                 public void run() {
-                    messageTextView.setText("Got "+o);
-                    // Toast.makeText(WaveClientSample.this, "Got "+o, Toast.LENGTH_LONG).show();
+                    messageTextView.setText(TextUtils.join("\n", logLines));
                 }
             });
         }
